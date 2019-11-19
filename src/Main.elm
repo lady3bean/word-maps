f85d67ed26f17a260c9667709059730c92a6e729
Anne Brett <annebrett@Annes-MacBook-Pro.local>
2019-11-19 12:09:16 -0500

    adds word lookup button and expands use of maintaining state in the model

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, pre, text, ul, li)
+import Html exposing (Html, button, div, input, li, pre, text, ul)
+import Html.Events exposing (onClick, onInput)
 import Http
 import Json.Decode as Decode exposing (Decoder, field, int, string)
 import Json.Decode.Field as Field
+
 
 
 --- CONSTANTS ---
@@ -18,7 +20,20 @@ apiUrl =
 ---- MODEL ----
 
 
-type Model
+type alias Model =
+    { lookupValue : String
+    , pageState : PageState
+    }
+
+
+initialModel : Model
+initialModel =
+    { lookupValue = ""
+    , pageState = Loading
+    }
+
+
+type PageState
     = Failure
     | Loading
     | Success BaseWordData
@@ -42,12 +57,14 @@ type alias BaseWordData =
     , language : Language
     }
 
+
 type alias RelatedWordData =
     { id : Int
     , definition : Maybe String
     , spelling : String
     , language_id : Int
     }
+
 
 type alias Language =
     { id : Int
@@ -56,24 +73,27 @@ type alias Language =
     }
 
 
+
 --- UPDATE ---
 
 
-
 type Msg
-    = GotWord (Result Http.Error BaseWordData)
+    = FetchWord
+    | GotWord (Result Http.Error BaseWordData)
+    | UpdateLookupValue String
+
 
 init : ( Model, Cmd Msg )
 init =
-    ( Loading
-    , getWord
+    ( initialModel
+    , Cmd.none
     )
 
 
-getWord : Cmd Msg
-getWord =
+fetchWord : String -> Cmd Msg
+fetchWord word =
     Http.get
-        { url = apiUrl ++ "wheel"
+        { url = apiUrl ++ word
         , expect = Http.expectJson GotWord baseWordDecoder
         }
 
@@ -81,73 +101,101 @@ getWord =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        FetchWord ->
+            ( { model | pageState = Loading }, fetchWord model.lookupValue )
+
         GotWord result ->
             case result of
                 Ok word ->
-                    ( Success word, Cmd.none )
+                    ( { model | pageState = Success word }, Cmd.none )
 
                 Err _ ->
-                    ( Failure, Cmd.none )
+                    ( { model | pageState = Failure }, Cmd.none )
+
+        UpdateLookupValue value ->
+            ( { model | lookupValue = value }, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
-    case model of
+    case model.pageState of
         Failure ->
-            text "Failed to load word data"
+            div []
+                [ text "Failed to load word data. Please look up another word."
+                , wordLookupInput
+                , wordLookupButton
+                ]
 
         Loading ->
-            text "Loading..."
+            div []
+                [ text "Loading... If you'd like, you can look up another word"
+                , wordLookupInput
+                , wordLookupButton
+                ]
 
         Success word ->
-            div [] [
-                    div [] [text ("spelling: " ++ word.spelling)]
-                    , div [] [text ("language: " ++ word.language.name)]
-                    , div []
-                        [text ("definition: " ++ (Maybe.withDefault "We seem to be missing this word's definition" word.definition))]
-                    , div []
-                        [
-                        text "origins: "
-                        , renderRelatedWordList word.origins
-                        ]
-                    , div []
-                        [
-                        text "origin of: "
-                        , renderRelatedWordList word.origin_ofs
-                        ]
-                    , div []
-                        [
-                        text "relations: "
-                        , renderRelatedWordList word.relations
-                        ]
-                    , div []
-                        [
-                        text "derivations: "
-                        , renderRelatedWordList word.derivations
-                        ]
-                    , div []
-                        [
-                        text "derived from: "
-                        , renderRelatedWordList word.derived_froms
-                        ]
+            div []
+                [ div [] [ text ("spelling: " ++ word.spelling) ]
+                , div [] [ text ("language: " ++ word.language.name) ]
+                , div []
+                    [ text ("definition: " ++ Maybe.withDefault "We seem to be missing this word's definition" word.definition) ]
+                , div []
+                    [ text "origins: "
+                    , renderRelatedWordList word.origins
                     ]
-
+                , div []
+                    [ text "origin of: "
+                    , renderRelatedWordList word.origin_ofs
+                    ]
+                , div []
+                    [ text "relations: "
+                    , renderRelatedWordList word.relations
+                    ]
+                , div []
+                    [ text "derivations: "
+                    , renderRelatedWordList word.derivations
+                    ]
+                , div []
+                    [ text "derived from: "
+                    , renderRelatedWordList word.derived_froms
+                    ]
+                , div []
+                    [ wordLookupInput
+                    , wordLookupButton
+                    ]
+                ]
 
 
 renderRelatedWordSpelling : RelatedWordData -> Html msg
 renderRelatedWordSpelling word =
     li [] [ text word.spelling ]
 
+
 renderRelatedWordList : List RelatedWordData -> Html msg
 renderRelatedWordList list =
     let
-        listItems = List.map renderRelatedWordSpelling list
+        listItems =
+            List.map renderRelatedWordSpelling list
     in
-        ul [] listItems
+    ul [] listItems
+
+
+wordLookupInput : Html Msg
+wordLookupInput =
+    div []
+        [ input [ onInput UpdateLookupValue ] []
+        ]
+
+
+wordLookupButton : Html Msg
+wordLookupButton =
+    button [ onClick FetchWord ] [ text "Look up" ]
+
+
 
 ---- PROGRAM ----
 
@@ -168,46 +216,59 @@ main =
 
 baseWordDecoder : Decoder BaseWordData
 baseWordDecoder =
-        Field.require "id" int <| \id ->
-        Field.attempt "definition" string <| \definition ->
-        Field.require "spelling" string <| \spelling ->
-        Field.require "language_id" int <| \language_id ->
-        Field.require "origins" relatedWordDataDecoder <| \origins ->
-        Field.require "language" languageDecoder <| \language ->
-        Field.require "origin_ofs" relatedWordDataDecoder <| \origin_ofs ->
-        Field.require "relations" relatedWordDataDecoder <| \relations ->
-        Field.require "derivations" relatedWordDataDecoder <| \derivations ->
-        Field.require "derived_froms" relatedWordDataDecoder <| \derived_froms ->
-
-        Decode.succeed
-            { id = id
-            , definition = definition
-            , spelling = spelling
-            , language_id = language_id
-            , origins = origins
-            , origin_ofs = origin_ofs
-            , relations = relations
-            , derivations = derivations
-            , derived_froms = derived_froms
-            , language = language
-            }
+    Field.require "id" int <|
+        \id ->
+            Field.attempt "definition" string <|
+                \definition ->
+                    Field.require "spelling" string <|
+                        \spelling ->
+                            Field.require "language_id" int <|
+                                \language_id ->
+                                    Field.require "origins" relatedWordDataDecoder <|
+                                        \origins ->
+                                            Field.require "language" languageDecoder <|
+                                                \language ->
+                                                    Field.require "origin_ofs" relatedWordDataDecoder <|
+                                                        \origin_ofs ->
+                                                            Field.require "relations" relatedWordDataDecoder <|
+                                                                \relations ->
+                                                                    Field.require "derivations" relatedWordDataDecoder <|
+                                                                        \derivations ->
+                                                                            Field.require "derived_froms" relatedWordDataDecoder <|
+                                                                                \derived_froms ->
+                                                                                    Decode.succeed
+                                                                                        { id = id
+                                                                                        , definition = definition
+                                                                                        , spelling = spelling
+                                                                                        , language_id = language_id
+                                                                                        , origins = origins
+                                                                                        , origin_ofs = origin_ofs
+                                                                                        , relations = relations
+                                                                                        , derivations = derivations
+                                                                                        , derived_froms = derived_froms
+                                                                                        , language = language
+                                                                                        }
 
 
 relatedWordDataDecoder : Decoder (List RelatedWordData)
 relatedWordDataDecoder =
-    Decode.list(
-        Field.require "id" int <| \id ->
-        Field.attempt "definition" string <| \definition ->
-        Field.require "spelling" string <| \spelling ->
-        Field.require "language_id" int <| \language_id ->
+    Decode.list
+        (Field.require "id" int <|
+            \id ->
+                Field.attempt "definition" string <|
+                    \definition ->
+                        Field.require "spelling" string <|
+                            \spelling ->
+                                Field.require "language_id" int <|
+                                    \language_id ->
+                                        Decode.succeed
+                                            { id = id
+                                            , definition = definition
+                                            , spelling = spelling
+                                            , language_id = language_id
+                                            }
+        )
 
-        Decode.succeed
-            { id = id
-            , definition = definition
-            , spelling = spelling
-            , language_id = language_id
-        }
-    )
 
 languageDecoder : Decoder Language
 languageDecoder =
